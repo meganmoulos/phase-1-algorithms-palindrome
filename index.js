2e1283cab7d0fbf2d507f45ff8d64ea2a3e8c144
Megan <meganmoulos@gmail.com>
2022-09-01 14:15:32 -0700

    changes

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ function isPalindrome(word) {
   // Write your algorithm here
   let splitString = word.split('')
   let reversedWord = splitString.reverse().join('')
-  if (word === reversedWord){
-    return true
-  } else {
-    return false
-  }
+  return (word === reversedWord) 
 }
 
 /* 
@@ -21,7 +17,8 @@ function isPalindrome(word) {
 
 /*
   Add written explanation of your solution here
-  The function takes a string as an argument and first splits it using the split() method. It then reverses the array created by split and joins the array back into a string. The last step compares the revered word string to the original string and returns true only if they are a match.
+  The function takes a string as an argument and first splits it using the split() method. It then reverses the array created by split and joins the array back into a string. 
+  The last step compares the revered word string to the original string and returns true only if they are a match.
 */
 
 // You can run `node index.js` to view these console logs
@@ -34,6 +31,9 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: true")
+  console.log("=>", isPalindrome("abba"))
 }
 
 module.exports = isPalindrome;
